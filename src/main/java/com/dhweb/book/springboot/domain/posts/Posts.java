1753a56f2bdf32d9e4d86cbdeb0bf9c4512d70a3
donghyun <kdhn7395@gmail.com>
2021-02-14 19:08:27 +0900

    Add Posts class and repository in domain package

---
+++ src/main/java/com/dhweb/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.dhweb.book.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity // 테이블과 링크될 클래임을 나타냄. 기본 값으로 CamelCase 이름을 under_score 네이밍으로 테이블 이름을 매칭함.
+public class Posts {
+
+    @Id // PK 선언
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increments 를 위함.
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
